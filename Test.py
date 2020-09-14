e3e2ce7591c793314579afcf38caf61c92ca19ce
baezien <71265850+baezien@users.noreply.github.com>
2020-09-14 21:15:50 +0900

    Create Test.py
    add new file

---
+++ Test.py
@@ -0,0 +1,8 @@
+stocks = {"계란":500,"아이스크림":100,"라면":200}
+keys = stocks.keys()
+values = stocks.values()
+items = stocks.items()
+
+print(keys,type(keys))
+print(values,type(values))
+print(items,type(items))
